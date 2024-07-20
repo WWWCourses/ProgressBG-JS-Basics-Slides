2ff47eca69976ef84ba904b8da7f7c1a996760e6
WWWCourses <ivawebcourses@gmail.com>
2024-07-20 14:05:46 +0300

    update

---
+++ pages/themes/JS_Arrays/HW/HW_Format Quiz Questions.js
@@ -0,0 +1,36 @@
+/* ---------------------------------- GIVEN --------------------------------- */
+// Array of questions
+let questions = [
+    "What is the capital of France?",
+    "Which language is used for Front-end Web development?",
+    "What does CSS stand for?"
+];
+
+// Array of options for each question
+let options = [
+    ["Berlin", "Madrid", "Paris", "Lisbon"],
+    ["Python", "JavaScript", "C++", "Java"],
+    ["Cascading Style Sheets", "Creative Style System", "Computer Style Sheets", "Colorful Style Sheets"]
+];
+
+// Array of correct answers
+let answers = [
+    "Paris",
+    "JavaScript",
+    "Cascading Style Sheets"
+];
+
+
+/* ----------------------------- YOUR CODE HERE ----------------------------- */
+
+
+/* ---------------------------------- TEST ---------------------------------- */
+console.log(output);
+
+/* ---------------------------- EXPECTED OUTPUT: ---------------------------- */
+// What is the capital of France? (Paris)
+//     Berlin, Madrid, Paris, Lisbon
+// Which language is used for Front-end Web development? (JavaScript)
+//     Python, JavaScript, C++, Java
+// What does CSS stand for? (Cascading Style Sheets)
+//     Cascading Style Sheets, Creative Style System, Computer Style Sheets, Colorful Style Sheets
