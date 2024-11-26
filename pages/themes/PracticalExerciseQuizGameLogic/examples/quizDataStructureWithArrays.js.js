7ea993ab3daac96bd2210107134f515a36a6ab93
WWWCourses <ivawebcourses@gmail.com>
2024-11-26 17:35:55 +0200

    update

---
+++ pages/themes/PracticalExerciseQuizGameLogic/examples/quizDataStructureWithArrays.js.js
@@ -0,0 +1,32 @@
+/* ---------------------------------- GIVEN --------------------------------- */
+// Define next questions in an appropriate data strucutre:
+
+    Q1: "What is the capital of France?",
+    Q2: "Which language is used for Front-end Web development?",
+    Q3: "What does CSS stand for?"
+
+// Define next options for each question
+
+    Q1 Options: "Berlin", "Madrid", "Paris", "Lisbon"
+    Q2 Options: "Python", "JavaScript", "C++", "Java"
+    Q3 Options: "Cascading Style Sheets", "Creative Style System", "Computer Style Sheets", "Colorful Style Sheets"
+
+
+// Define correct answers for each question
+    Q1 Answer: "Paris",
+    Q2 Answer: "JavaScript",
+    Q3 Answer: "Cascading Style Sheets"
+
+// Define output variable which will contain a next string, using the data in questions, options and answers
+
+
+/* ---------------------------------- TEST ---------------------------------- */
+console.log(output);
+
+/* ---------------------------- EXPECTED OUTPUT: ---------------------------- */
+// What is the capital of France? (Paris)
+//     Berlin, Madrid, Paris, Lisbon
+// Which language is used for Front-end Web development? (JavaScript)
+//     Python, JavaScript, C++, Java
+// What does CSS stand for? (Cascading Style Sheets)
+//     Cascading Style Sheets, Creative Style System, Computer Style Sheets, Colorful Style Sheets
