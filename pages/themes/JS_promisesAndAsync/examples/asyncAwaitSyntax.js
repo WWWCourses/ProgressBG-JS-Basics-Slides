d7448dc47fe725b28184701a377033acd0aae01d
WWWCourses <ivawebcourses@gmail.com>
2025-01-28 18:58:39 +0200

    update

--- pages/themes/JS_promisesAndAsync/examples/asyncAwaitSyntax.js
+++ pages/themes/JS_promisesAndAsync/examples/asyncAwaitSyntax.js
@@ -3,15 +3,10 @@ async function square(x) {
 	return x**2
 }
 
-async function add(x) {
-	console.log(`calc sum1`);
-	sum1 = await square(3)
-
-	console.log(`calc sum2`);
-	sum2 = await square(3)
-
-	console.log(`Total sum: ${sum1+sum2}`);
-
+async function calculate() {
+    const sum1 = await square(3); // Waits for the result of square(3)
+    const sum2 = await square(3); // Waits for the result of square(3)
+    console.log(`Total sum: ${sum1 + sum2}`);
 }
 
 function print(msg) {
@@ -19,7 +14,7 @@ function print(msg) {
 }
 
 console.log(`1`);
-add(3)
+calculate(3)
 console.log(`2`);
 
 /* ------------------------------ more insights ----------------------------- */
