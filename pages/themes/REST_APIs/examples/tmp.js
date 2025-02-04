b7261b589e390e6e77c24b3ac24c1cd0d0bb54e0
WWWCourses <ivawebcourses@gmail.com>
2025-02-04 18:19:04 +0200

    update

--- pages/themes/REST_APIs/examples/tmp.js
+++ pages/themes/REST_APIs/examples/tmp.js
@@ -1,11 +1,12 @@
-async function getPost() {
-    try {
-        const response = await fetch('https://jsonplaceholder.typicode.com/posts/1');
-        const data = await response.json();
-        console.log(data);
-    } catch (error) {
-        console.error('Error:', error);
-    }
-}
-
-getPost();
+fetch('https://jsonplaceholder.typicode.com/posts/1', {
+    method: 'PATCH',
+    body: JSON.stringify({
+        title: 'Partially Updated Post',
+    }),
+    headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+    },
+})
+    .then((response) => response.json())
+    .then((data) => console.log(data))
+    .catch(error => console.error('Error:', error));
